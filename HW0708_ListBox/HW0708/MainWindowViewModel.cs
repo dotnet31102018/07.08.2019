01b4995191c6b948da0b91610cf219b21f35df15
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-08-07 19:41:31 +0300

    Add files via upload

---
+++ HW0708_ListBox/HW0708/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HW0708
+{
+    public class MainWindowViewModel 
+    {
+        public ICommand AddPersonCommand { get; set; }
+        public ObservableCollection<Person> People { get; set; }
+        public Person NewPerson { get; set; }
+        public MainWindowViewModel()
+        {
+            NewPerson = new Person();
+            People = new ObservableCollection<Person>()
+            {
+                new Person { Age = 12, Name = "Danny", Smoker = false},
+                new Person { Age = 28, Name = "Moshe", Smoker = true},
+                new Person { Age = 25, Name = "Suzi", Smoker = false},
+                new Person { Age = 50, Name = "Safta", Smoker = true}
+            };
+            AddPersonCommand = new DelegateCommand(
+                () => {
+                    People.Add(NewPerson);
+                }
+                );
+        }
+
+    }
+}
---
+++ HW0708_ListBox/HW0708/Person.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW0708
+{
+    public class Person : INotifyPropertyChanged
+    {
+        private string name;
+        private int age;
+        private bool smoker;
+        public Action method { get; set; }
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+                OnPropertyChanged("Name");
+                if (method != null)
+                {
+                    method.Invoke();
+                }
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                this.age = value;
+                OnPropertyChanged("Age");
+            }
+        }
+        public bool Smoker
+        {
+            get
+            {
+                return this.smoker;
+            }
+            set
+            {
+                this.smoker = value;
+                OnPropertyChanged("Smoker");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Person {Name} {Age} Smoking? (bad for you) {Smoker}";
+        }
+    }
+
+
+}
